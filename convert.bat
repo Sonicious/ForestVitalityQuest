7173f2ff95d272398e6df570c1156f0e7d6e649e
Martin Reinhardt <martin.reinhardt.3@uni-leipzig.de>
2024-04-24 16:36:21 +0200

    added better compression and more versatile approach

--- convert.bat
+++ convert.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Set the path to your "SN" folder
-set "sourceDir=C:\Users\marti\workspace\ForestVitalityQuest\data\MD"
+REM Set the path to your tiff folder
+set "sourceDir=PATH"
 
 REM Set the path to the target directory where the NetCDF files will be saved
-set "targetDir=C:\Users\marti\workspace\ForestVitalityQuest\data\MD_NC"
+set "targetDir=PATH"
 
 REM Create the target directory if it doesn't exist
 if not exist "!targetDir!" mkdir "!targetDir!"
@@ -19,7 +19,7 @@ for %%f in (*.tif) do (
     set "outputName=!targetDir!\%%~nf.nc"
     
     REM Combine gdalwarp and gdal_translate to reproject and convert directly to NetCDF
-    gdalwarp -of NetCDF -co "FORMAT=NC4C" "%%f" "!outputName!"
+    gdalwarp -of NetCDF -co "FORMAT=NC4C" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" "%%f" "!outputName!"
     REM gdalwarp -t_srs EPSG:3035 -of NetCDF -co "FORMAT=NC4C" -co "COMPRESS=DEFLATE" "%%f" "!outputName!"
 )
 
